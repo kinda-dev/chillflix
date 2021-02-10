b8a67ef535482aa4c13b884398c1a3a7a29f3a09
kinda-dev <maybe.fabio@gmail.com>
2021-02-09 21:59:47 -0800

    add users reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,17 @@
+import  { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const usersReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+
+    const nextState = Object.assign({}, oldState);
+
+    switch(action.type) {
+        case RECEIVE_CURRENT_USER:
+            nextState[action.currentUser.id] = action.currentUser;
+            return nextState;
+        default:
+            return oldState;
+    }
+};
+
+export default usersReducer;
