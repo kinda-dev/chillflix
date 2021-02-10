eeba5671f2bb7e9cffe7d9dfef83be84325f0f05
kinda-dev <maybe.fabio@gmail.com>
2021-02-09 22:24:25 -0800

    add errors reducer and root reducer

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
   
-  export default (state = [], action) => {
+const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
       case RECEIVE_SESSION_ERRORS:
@@ -11,3 +11,5 @@ import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session
         return state;
     }
   };
+
+  export default sessionErrorsReducer;
