abeef48a864c6bbe79f16eefaf9017a8aa65da03
kinda-dev <maybe.fabio@gmail.com>
2021-02-10 16:20:15 -0800

    finish login form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import { Link, Route } from 'react-router-dom';
 import Test from './dummy_auth_test/test_auth_container';
+import LoginFormContainer from './session_form/login_form_container';
 
 const App = () => (
     <div>
         <header>
-            <h1>Work In Progress</h1>
+            <Link to="/" className="header-link">
+                <h1>Work In Progress</h1>
+            </Link>
             <Test />
         </header>
+        <Route path="/login" component={LoginFormContainer} />
     </div>
 );
 
---
+++ frontend/components/session_form/login_form.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+
+import { login } from '../../actions/session_actions'
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+    }
+
+    handleEmail(e) {
+        this.setState({email: e.target.value})
+    }
+
+    handlePassword(e) {
+        this.setState({password: e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.login(user);
+    }
+
+    renderErrors() {
+        return(
+            <ul className="login-errors">
+                {this.props.errors.map((error, i) => (
+                    <li key={i}>{error}</li>
+                ))}
+            </ul>
+        );
+    }
+
+    render() {
+        return(
+            <div className="login-form-container">
+                <form onSubmit={this.handleSubmit} className="login-form-box">
+                    <h2 className="sign-in-letters">Sign In</h2>
+                    <br/>
+                    <input className="login-input" placeholder="Email" type="text" value={this.state.email} onChange={this.handleEmail} />
+                    <br/>
+                    <input className="login-input" placeholder="Password" type="password" value={this.state.password} onChange={this.handlePassword}/>
+                    <br/>
+                    {this.renderErrors}
+                    <br/>
+                    <button className="login-button">Sign In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
