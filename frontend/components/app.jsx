c9be1709a37f3ce4ad798b3c3179ac5c268b59ad
kinda-dev <maybe.fabio@gmail.com>
2021-02-11 11:33:37 -0800

    insert logo create AuthRoute fix App.jsx Switch components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
-import { Link, Route } from 'react-router-dom';
 import Test from './dummy_auth_test/test_auth_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import PreSignupFormContainer from './session_form/pre_signup_form';
-
+import { AuthRoute } from '../util/route_util';
 
 const App = () => {
  return(
     <div>
         <header>
             <Link to="/" className="header-link">
-                <h1>Work In Progress</h1>
+                <img src={window.logoImage} alt="Chillflix"/>
+                {/* <h1>Work In Progress</h1> */}
             </Link>
             {/* line below and logic related to fix, will be used in upper corner of navbar */}
             <Test />
         </header>
-        <PreSignupFormContainer />
-        <Route exact path="/login" component={LoginFormContainer} />
-        <Route exact path="/signup" component={SignupFormContainer} />
+                <Switch>
+                    <AuthRoute exact path="/" component={PreSignupFormContainer} />
 
+                    {/* <PreSignupFormContainer /> */}
+                    <AuthRoute exact path="/login" component={LoginFormContainer} />
+                    <AuthRoute exact path="/signup" component={SignupFormContainer} />
+                </Switch>
         
             {/* <img src={window.logoURL} /> double check*/}
 
