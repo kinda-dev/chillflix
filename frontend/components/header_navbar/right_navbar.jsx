7c79a17cbcaa29a904f65674b4479cac57dfa828
kinda-dev <maybe.fabio@gmail.com>
2021-02-12 01:10:04 -0800

    heroku update

--- frontend/components/header_navbar/right_navbar.jsx
+++ frontend/components/header_navbar/right_navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RightNavbar = ({ currentUser, logout }) => {
-    const loginOrSignupLinks = () => (
+    const loginLink = () => (
         <nav className="right-navbar-logged-out">
             <Link to="/login"><button className="login-button">Sign In</button></Link>
             {/* break line under here to fix, this should work: &nbsp;or&nbsp; */}
@@ -10,7 +10,7 @@ const RightNavbar = ({ currentUser, logout }) => {
         </nav>
     );
 
-    const sayHi = () => (
+    const rightNav = () => (
         <nav className="right-navbar-logged-in">
             <img className="profileImg" src={window.profileImage} alt="ProfileImg"/>
             <div className="hover-menu">
@@ -19,7 +19,7 @@ const RightNavbar = ({ currentUser, logout }) => {
         </nav>
     );
 
-    return currentUser ? sayHi() : loginOrSignupLinks();
+    return currentUser ? rightNav() : loginLink();
 };
 
 export default RightNavbar;
