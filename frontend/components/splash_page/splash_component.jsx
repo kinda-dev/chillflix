e539fb7cea902830748be409befa0b434747c75c
kinda-dev <maybe.fabio@gmail.com>
2021-02-14 18:55:33 -0800

    work on splash page style

--- frontend/components/splash_page/splash_component.jsx
+++ frontend/components/splash_page/splash_component.jsx
@@ -34,8 +34,6 @@ class SplashComponent extends  React.Component {
         return(
             <div className="splash-outer-wrap">
 
-                <div className="hero">
-                
                     <header className="splash-header">
                         <div className="splash-header-wrapper">
                             <Link to="/" className="splash-header-home-link">
@@ -46,47 +44,67 @@ class SplashComponent extends  React.Component {
                         </div>
                     </header>
 
+                <div className="splash-sections">
 
-                    <div className="splash-second-wrapper">
-                        <div className="pre-signup-form-container">
+                    <div className="hero">
 
-                            <div className="pre-signup-form-text">
-                                <h2 className="splash-first-sentence">Unlimited movies, TV shows, and more.</h2>
-                                <h4 className="splash-second-sentence">Watch anywhere. Cancel anytime.</h4>
-                                <h4 className="splash-third-sentence">Ready to watch? Enter your email to create 
-                                or restart your membership.</h4>
+                        <div className="hero-background"></div>
+
+                         <div className="pre-signup-form-container">
+                             {/* <div className="pre-signup-form-text"> */}
+                                 <h2 className="splash-first-sentence">Unlimited movies, TV shows, and more.</h2>
+                                 <h4 className="splash-second-sentence">Watch anywhere. Cancel anytime.</h4>
+                             {/* </div> */}
+
+                             <form onSubmit={this.handleSubmit} className="pre-signup-form-box">
+                                 
+                                 <h4 className="splash-third-sentence">Ready to watch? Enter your email to create 
+                                 or restart your membership.</h4>
+                                 <br/>
+
+                                 <div className="pre-signup-inputs-lockup">
+                                    <input className="pre-signup-input" placeholder="Email address" type="text" value={this.state.email} onChange={this.handleEmail} />
+                                    <br/>
+                                    {/* <div className="pre-signup-button-container"> */}
+                                        <Link to="/signup">
+                                            <button className="pre-signup-button" ><span className="pre-signup-button-text">Get Started {'>'}</span></button>
+                                        </Link>
+                                    {/* </div> */}
+                                 </div>
+
+                             </form>
+                             <div className="random-obj"></div>
+                         </div>
+
+                         <div className="center-pixel"></div>
+
+                    </div>
+
+                    <section className="splash-television-element">
+
+                        <div className="splash-television-first-child">
+                            
+                            <div className="splash-television-text-container">
+                                <h2 className="splash-first-sentence">Enjoy on your TV.</h2>
+                                <h3 className="splash-second-sentence">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h3>
                             </div>
 
-                            <form onSubmit={this.handleSubmit} className="pre-signup-form-box">
-                                <br/>
-                                <input className="pre-signup-input" placeholder="Email address" type="text" value={this.state.email} onChange={this.handleEmail} />
-                                <br/>
-                                <div className="pre-signup-button-container">
-                                    <Link to="/signup">
-                                        <button className="pre-signup-button" >Get Started ></button>
-                                    </Link>
+                            <div className="splash-television-image-container">
+                                <div className="splash-television-animation-container">
+                                    <img className="tv-img" src={window.tvImage} alt="televisionImage"/>
+                                    <div className="splash-videoanimation">
+                                        <video className="splash-video" controls autoPlay muted><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"></source></video>
+                                        <div className="splash-television-animation-text"></div>
+                                    </div>
                                 </div>
-                            </form>
+                                <div className="center-pixel"></div>
+                            </div>
 
                         </div>
-                    </div>
 
-                </div>
-           
+                    </section>
 
-
-                <div className="splash-television-element">
-
-                    <div className="first-splash-text">
-                        <h2 className="splash-first-sentence">Enjoy on your TV.</h2>
-                        <h3 className="splash-second-sentence">Watch on Smart TVs, Playstation, Xbox,</h3>
-                        <h4 className="splash-third-sentence">Chromecast, Apple TV, Blu-ray players, and more.</h4>
-                    </div>
-
-                    <div className="splash-television-img">
-                        <img className="tv-img" src={window.tvImage} alt="televisionImage"/>
-                    </div>
-                </div>
+                </div>   
 
             </div>
 
