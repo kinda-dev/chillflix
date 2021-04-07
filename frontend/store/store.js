3370910d1ae1e9fb427f41df9eb9bb0acbaa0997
kinda-dev <maybe.fabio@gmail.com>
2021-04-07 13:01:17 -0700

    fix timeout

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,8 +3,15 @@ import rootReducer from "../reducers/root_reducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger)
+    createStore(rootReducer, preloadedState, applyMiddleware(...middlewares)
     );
 
 export default configureStore;
